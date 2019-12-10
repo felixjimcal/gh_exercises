7eb1ead9210c922a20f8d51c4b27e0936ded72be
felixjimcal <felixjcalvo@gmail.com>
2019-12-10 09:52:09 +0100

    More examples

--- arrays.rb
+++ arrays.rb
@@ -1,4 +1,5 @@
 letras = ["a","b",3,"4"]
+numeros = 1..5 #generate array of numbers automaticaly
 
 puts "for loop"
 for i in letras do
@@ -9,3 +10,21 @@ puts "for_each loop"
 letras.each do |element|
     puts element
 end
+
+puts "print elements"
+puts letras[0]
+puts letras[-1]
+
+puts "for_each loop of numbers"
+numeros.each do |element|
+    puts element
+end
+
+puts numeros.include? 7
+print "Pares: "
+print numeros.select(&:even?)
+puts ""
+print "Impares: "
+print numeros.select(&:odd?)
+puts ""
+print numeros.sum
